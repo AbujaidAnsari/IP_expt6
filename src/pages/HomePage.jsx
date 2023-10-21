1e42193d4cadfd81ede267c45a85790000de1ea5
AbujaidAnsari <113976174+AbujaidAnsari@users.noreply.github.com>
2023-10-21 11:16:53 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Section from "../components/Section/Section.jsx";
-import ProfileInfo from "../components/ProfileInfo/ProfileInfo";
+import Info from "../components/Info.jsx";
+
 const Home = () => {
   return (
     <>
       <Navbar />
       <Section/>
-      <div >
-        <ProfileInfo
-          name={"GitHub"}
-          username={"AbujaidAnsari"}
-          email={"Abujaid@gmail.com"}
-          link={""}
-        />
-        <ProfileInfo
-          name={"Instagram"}
-          username={"Abujaid"}
-          email={"Abujaid@gmail.com"}
-          link={"https://www.instagram.com"}
-        />
-        <ProfileInfo
-          name={"LinkedIn"}
-          username={"Abujaid"}
-          email={"Abujaid@gmail.com"}
-          link={""}
-        />
-      </div>
+      <Info name="Abujaid" division="D15B"/>
+      
     </>
   );
+
 };
+
+
+
 export default Home;
